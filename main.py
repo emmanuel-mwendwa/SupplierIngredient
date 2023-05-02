9e73b750fbd467e359f02d50555bca73f0cc91ac
Gibson-Gichuru <gichuru.developer@gmail.com>
2023-05-02 22:28:45 +0300

    done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from flask_wtf.form import _Auto
@@ -6,6 +6,7 @@ from wtforms import SubmitField, StringField, IntegerField, SelectField, SelectM
 from wtforms.validators import DataRequired
 import os
 import datetime
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -17,6 +18,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+migrate = Migrate(app=app, db=db)
 
 class SupplierForm(FlaskForm):
     name = StringField('Supplier Name: ')
@@ -55,9 +57,10 @@ class SupplierIngredientForm(FlaskForm):
 class SupplierIngredient(db.Model):
     __tablename__ = "supplier_ingredients"
 
-    supplier_id = db.Column(db.Integer, db.ForeignKey('suppliers.id'), primary_key=True)
-    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredients.id'), primary_key=True)
-
+    id = db.Column(db.Integer, primary_key=True)
+    supplier_id = db.Column(db.Integer, db.ForeignKey('suppliers.id'))
+    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredients.id'))
+    unit_cost = db.Column(db.Integer)
     supplier = db.relationship('Supplier', backref=db.backref('supplier_ingredients', cascade='all, delete-orphan'))
     ingredient = db.relationship('Ingredient', backref=db.backref('supplier_ingredients', cascade='all, delete-orphan'))
 
@@ -79,7 +82,7 @@ class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
     unit_of_measurement = db.Column(db.String(12))
-    unit_cost = db.Column(db.Integer)
+    
     created_at = db.Column(db.DateTime(), default=datetime.datetime.utcnow())
     updated_at = db.Column(db.DateTime(), default=datetime.datetime.utcnow())
 
@@ -100,8 +103,7 @@ def add_ingredient():
     if form.validate_on_submit():
         ingredient = Ingredient(
             name=form.name.data, 
-            unit_of_measurement=form.measurement.data,
-            unit_cost=form.unit_cost.data
+            unit_of_measurement=form.measurement.data
             )
         db.session.add(ingredient)
         db.session.commit()
@@ -124,25 +126,55 @@ def add_supplier():
         return redirect(url_for('index'))
     return render_template("add_supplier.html", form=form)
 
-@app.route("/add_supplier_ingredient", methods=["GET", "POST"])
+@app.route("/add_supplier_ingredient", methods=["GET"])
 def add_supplier_ingredient():
-    form = SupplierIngredientForm()
-    if form.validate_on_submit():
-        supplier_id = form.supplier.data
-        ingredients = Ingredient.query.all()
-        selected_ingredients = []
-        for ingredient in ingredients:
-            if ingredient.id in form.ingredients.data:
-                selected_ingredients.append(ingredient)
-                supplier_ingredient = SupplierIngredient(
-                    supplier_id=supplier_id, 
-                    ingredient_id=ingredient.id
-                    )
-                db.session.add(supplier_ingredient)
-        db.session.commit()
-        flash('Supplier Ingredient added successfully')
-        return redirect(url_for('index'))
-    return render_template('supplier_ingredient.html', form=form)
+    ingredients = [ingredient.name for ingredient in Ingredient.query.all()]
+
+    suppliers = [supplier.name for supplier in Supplier.query.all()]
+   
+    return render_template('supplier_ingredient.html', suppliers=suppliers, ingredients=ingredients)
+
+@app.route("/add_supplier_ingredient", methods=["POST"])
+def post_supplier_ingredient():
+
+    data = request.get_json()
+
+    supplier = Supplier.query.filter_by(name=data.get('supplier')).first()
+
+    if supplier is None:
+
+        return jsonify({"message":"supplier not found"}), 404
+
+
+    ingreditents = [
+
+        Ingredient.query.filter_by(
+        name = ingredient
+        ).first() for ingredient in list(
+            filter(
+                lambda x: x != 'supplier', 
+                data.keys()
+            )
+        )
+    ]
+
+    for ingredient in ingreditents:
+
+        if ingredient is not None:
+
+            supplier_ingredient = SupplierIngredient()
+
+            supplier_ingredient.supplier = supplier
+
+            supplier_ingredient.ingredient = ingredient
+
+            supplier_ingredient.unit_cost = data.get(ingredient.name)
+
+            db.session.add(supplier_ingredient)
+
+    db.session.commit()
+
+    return jsonify({"message":"pk"}), 201
             
 
 if __name__ == "__main__":
